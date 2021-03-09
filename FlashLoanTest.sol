099bf7ac84f2a3e308c3fb3927c7152246e02484
ZinCoin <34787030+ZinCoin@users.noreply.github.com>
2021-03-09 09:40:13 -0800

    Update FlashLoanTest.sol

--- FlashLoanTest.sol
+++ FlashLoanTest.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity 0.6.12;
 
-import { FlashLoanReceiverBase } from "./FlashLoanReceiverBase.sol";
-import { ILendingPool } from "./ILendingPool.sol";
-import { ILendingPoolAddressesProvider } from "./ILendingPoolAddressesProvider.sol";
-import { IERC20 } from "./IERC20.sol";
+import { FlashLoanReceiverBase } from "FlashLoanReceiverBase.sol";
+import { ILendingPool, ILendingPoolAddressesProvider, IERC20 } from "Interfaces.sol";
+import { SafeMath } from "Libraries.sol";
+import {IFlashLoanReceiver} from "IFlashLoanReceiver.sol";
 
 /** 
     !!!
@@ -12,7 +12,7 @@ import { IERC20 } from "./IERC20.sol";
     exposed to a 'griefing' attack, where the stored funds are used by an attacker.
     !!!
  */
-contract MyV2FlashLoan is FlashLoanReceiverBase {
+contract FlashLoanTest is FlashLoanReceiverBase {
     using SafeMath for uint256;
 
     constructor(ILendingPoolAddressesProvider _addressProvider) FlashLoanReceiverBase(_addressProvider) public {}
@@ -36,7 +36,8 @@ contract MyV2FlashLoan is FlashLoanReceiverBase {
         // This contract now has the funds requested.
         // Your logic goes here.
         //
-
+        
+        
         // At the end of your logic above, this contract owes
         // the flashloaned amounts + premiums.
         // Therefore ensure your contract has enough to repay
@@ -55,17 +56,32 @@ contract MyV2FlashLoan is FlashLoanReceiverBase {
         address receiverAddress = address(this);
 
         address[] memory assets = new address[](7);
-        assets[0] = address(0x2E0326ab7088f2256aE73fb3032987A99D375413); // Kovan AAVE
-
+        assets[0] = address(0xB597cd8D3217ea6477232F9217fa70837ff667Af); // Kovan AAVE
+        assets[1] = address(0x2d12186Fbb9f9a8C28B3FfdD4c42920f8539D738); // Kovan BAT
+        assets[2] = address(0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD); // Kovan DAI
+        assets[3] = address(0x075A36BA8846C6B6F53644fDd3bf17E5151789DC); // Kovan UNI
+        assets[4] = address(0xb7c325266ec274fEb1354021D27FA3E3379D840d); // Kovan YFI
+        assets[5] = address(0xAD5ce863aE3E4E9394Ab43d4ba0D80f419F61789); // Kovan LINK
+        assets[6] = address(0x7FDb81B0b8a010dd4FFc57C3fecbf145BA8Bd947); // Kovan SNX
 
         uint256[] memory amounts = new uint256[](7);
         amounts[0] = 1 ether;
-       
+        amounts[1] = 1 ether;
+        amounts[2] = 1 ether;
+        amounts[3] = 1 ether;
+        amounts[4] = 1 ether;
+        amounts[5] = 1 ether;
+        amounts[6] = 1 ether;
 
         // 0 = no debt, 1 = stable, 2 = variable
-        uint256[] memory modes = new uint256[](7);
+        uint256[] memory modes = new uint256[](1);
         modes[0] = 0;
-       
+        modes[1] = 0;
+        modes[2] = 0;
+        modes[3] = 0;
+        modes[4] = 0;
+        modes[5] = 0;
+        modes[6] = 0;
 
         address onBehalfOf = address(this);
         bytes memory params = "";
